a55f07e44b324777c2c8ccdcaac3389b85aaec48
Siraj Choudhry <sirajchoudhry@SmackBook-Pro.local>
2021-11-01 02:08:32 +0000

    initial enter address function

--- scraper.py
+++ scraper.py
@@ -8,19 +8,33 @@ import time
 
 class Scraper:
     
-    def __init__(self) -> None:
-        self.driver = webdriver.Chrome()
-        self.driver.get('https://deliveroo.co.uk')
-        self.__accept_cookies()
+    def __init__(self, address) -> None:
+        for place in address:
+            self.driver = webdriver.Chrome()
+            self.driver.get('https://deliveroo.co.uk')
+            self.__accept_cookies()
+            self.__enter_address(place)   ### Only works if 'mark location' button does not require the location pin to be moved
 
     def __accept_cookies(self):
         time.sleep(0.1)   ##could have a shorter sleep time
         self.driver.find_element(By.XPATH,'//*[@id="onetrust-accept-btn-handler"]').click()
 
     def __enter_address(self, address):
-        # TODO
-        pass
-    
+        self.addressbar = self.driver.find_element(By.XPATH, '//*[@id="location-search"]')
+        self.addressbar.send_keys(f'{address}')
+        self.driver.find_element(By.XPATH, '//*[@id="__next"]/div/div/div[2]/div[2]/div[3]/div/div[1]/div/div[2]/div/div/div/div/div/div[2]/span/button').click()
+        time.sleep(0.5)
+        try:
+            self.driver.find_element(By.XPATH, '/html/body/div[10]/div/div/div/div[2]/div/span/button').click() 
+        except:  ##if map location does not show 
+            pass
+
+#%%
+
+buck = ['emirates stadium', 'buckingham palace']
+example = Scraper(buck)
+#%%
+
     def __acknowledge_voucher(self):
         # TODO
         pass
