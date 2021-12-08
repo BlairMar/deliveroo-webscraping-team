95e1a77e4fcb6012bfbef5e9d8cb3a3d50605771
Siraj Choudhry <sirajchoudhry@SmackBook-Pro.local>
2021-12-08 23:47:36 +0000

    minimum charge and closing time columns added

--- Data_Processing.py
+++ Data_Processing.py
@@ -11,11 +11,39 @@ PASSWORD = 'password'
 DATABASE = 'DeliverooScrape'
 PORT = 5432
 
-df = pd.read_json('data/LS12 5NJ/data.json')
-df.columns = ['tags', 'name', 'image_path','uuid', 'url']
-df['tags'] = df['tags'].str.join(', ')
-df['tags'] = df['tags'].str.replace('£', '')
+address = 'SW1A 0AA'
+
+df = pd.read_json(f'data/{address}/data.json')
+df.columns = ['tags', 'name', 'image_path','uuid', 'url', 'rating']
+# df['tags'] = df['tags'].str.replace('£', '')
+# minimum_charge = []
+# for tags in df['tags']:
+#     for strings in tags:
+#         if 'minimum' in strings:
+#             strings.replace('£', '')
+#             minimum_charge.append(strings)
+#             # minimum_charge = df.loc[strings]
+#             # print(minimum_charge)
+# print(minimum_charge)
+
+# min_charge = [[strings for strings in tags if 'minimum' in strings] for tags in df['tags']]
+# df['minimum_charge'] = min_charge
+
+def my_func(my_list):
+    if not my_list:
+        return None
+    else:
+        return my_list[0]
+
+min_charge = [my_func([strings for strings in tags if 'minimum' in strings]) for tags in df['tags']]
+df['minimum_charge'] = min_charge
+df['minimum_charge'] = df['minimum_charge'].str.replace('£', '')
+
+closing = [my_func([strings for strings in tags if 'Closes at' in strings or 'Open until' in strings]) for tags in df['tags']]
+df['closing_time'] = closing
+
+
 
 engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
-df.to_sql('deliveroo_data',engine, if_exists='replace')
+df.to_sql(f'{address}',engine, if_exists='replace')
 # %%
