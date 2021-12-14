23f6a3f0d09d51d5f4dae5f5c187b040b570fe2f
Siraj Choudhry <sirajchoudhry@SmackBook-Pro.local>
2021-12-14 20:35:15 +0000

    most tags sorted

--- Data_Processing.py
+++ Data_Processing.py
@@ -1,5 +1,6 @@
 #%%
 
+from numpy import NaN
 from sqlalchemy import create_engine
 import pandas as pd
 
@@ -7,7 +8,7 @@ DATABASE_TYPE = 'postgresql'
 DBAPI = 'psycopg2'
 HOST = 'localhost'
 USER = 'postgres'
-PASSWORD = 'password'
+PASSWORD = 'ElSiraajy92!'
 DATABASE = 'DeliverooScrape'
 PORT = 5432
 
@@ -26,24 +27,62 @@ df.columns = ['tags', 'name', 'image_path','uuid', 'url', 'rating']
 #             # print(minimum_charge)
 # print(minimum_charge)
 
-# min_charge = [[strings for strings in tags if 'minimum' in strings] for tags in df['tags']]
-# df['minimum_charge'] = min_charge
-
 def my_func(my_list):
     if not my_list:
         return None
     else:
         return my_list[0]
 
-min_charge = [my_func([strings for strings in tags if 'minimum' in strings]) for tags in df['tags']]
-df['minimum_charge'] = min_charge
-df['minimum_charge'] = df['minimum_charge'].str.replace('£', '')
+pd.options.mode.chained_assignment = None
+
+rating = [my_func([strings for strings in tags if 'Excellent' in strings or 'Very good' in strings]) for tags in df['tags']]
+# [df["rating"].fillna(rat, inplace=True) 
+# #   for rat in rating for tags in df['tags']if rat in tags]
+[tags.remove(rat) for rat in rating for tags in df['tags'] if rat in tags]
+missing_rating = pd.isnull(df["rating"])
+df["rating"][missing_rating] = rating
+[tags.remove(rate) for tags in df['tags'] for rate in tags if ')' in rate and '(' in rate]
+
+
+min_spend = [my_func([strings for strings in tags if 'minimum' in strings]) for tags in df['tags']]
+[tags.remove(spend) for spend in min_spend for tags in df['tags'] if spend in tags]
+df['minimum_spend'] = min_spend
+df['minimum_spend'] = df['minimum_spend'].str.replace('£', '')
 
 closing = [my_func([strings for strings in tags if 'Closes at' in strings or 'Open until' in strings]) for tags in df['tags']]
+[tags.remove(time) for time in closing for tags in df['tags'] if time in tags]
 df['closing_time'] = closing
 
+opening = [my_func([strings for strings in tags if 'Opens at' in strings]) for tags in df['tags']]
+[tags.remove(opens) for opens in opening for tags in df['tags'] if opens in tags]
+df['opening_time'] = opening
 
+distance = [my_func([strings for strings in tags if 'miles away' in strings or 'mile away' in strings]) for tags in df['tags']]
+[tags.remove(dist) for dist in distance for tags in df['tags'] if dist in tags]
+df['distance'] = distance
+
+delivery_charge = [my_func([strings for strings in tags if 'delivery' in strings]) for tags in df['tags']]
+[tags.remove(charge) for charge in delivery_charge for tags in df['tags'] if charge in tags]
+df['delivery_charge'] = delivery_charge
+
+delivery_time = [my_func([strings for strings in tags if '-' in strings or 'min' in strings]) for tags in df['tags']]
+[tags.remove(long) for long in delivery_time for tags in df['tags'] if long in tags]
+df['delivery_time'] = delivery_time
+
+[tags.remove('View map') for tags in df['tags'] if 'View map' in tags]
+[tags.remove('Info') for tags in df['tags'] if 'Info' in tags]
+[tags.remove(val) for tags in df['tags'] for val in tags if 
+    'Road' in val or 
+    'Yards' in val or 
+    'Avenue' in val or 
+    'Street' in val or 
+    'Rd' in val or 
+    'Place' in val or 
+    'Walk' in val or 
+    'Lane' in val]
 
 engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
 df.to_sql(f'{address}',engine, if_exists='replace')
+# %%
+
 # %%
