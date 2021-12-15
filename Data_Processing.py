f9d83cfc3b91c8f9dcd8d2cbc08977a9d8ae7252
EddieEvans1996 <evane026@gmail.com>
2021-12-15 16:44:04 +0000

    adds tags refactoring to data processing

--- Data_Processing.py
+++ Data_Processing.py
@@ -8,24 +8,15 @@ DATABASE_TYPE = 'postgresql'
 DBAPI = 'psycopg2'
 HOST = 'localhost'
 USER = 'postgres'
-PASSWORD = 'password'
+PASSWORD = 'FzA2609042102Ege'
 DATABASE = 'DeliverooScrape'
 PORT = 5432
 
-address = 'SW1A 0AA'
+address = 'BS1 4ER'
 
 df = pd.read_json(f'data/{address}/data.json')
 df.columns = ['tags', 'name', 'image_path','uuid', 'url', 'rating']
-# df['tags'] = df['tags'].str.replace('£', '')
-# minimum_charge = []
-# for tags in df['tags']:
-#     for strings in tags:
-#         if 'minimum' in strings:
-#             strings.replace('£', '')
-#             minimum_charge.append(strings)
-#             # minimum_charge = df.loc[strings]
-#             # print(minimum_charge)
-# print(minimum_charge)
+
 
 def my_func(my_list):
     if not my_list:
@@ -69,20 +60,38 @@ delivery_time = [my_func([strings for strings in tags if '-' in strings or 'min'
 [tags.remove(long) for long in delivery_time for tags in df['tags'] if long in tags]
 df['delivery_time'] = delivery_time
 
-[tags.remove('View map') for tags in df['tags'] if 'View map' in tags]
-[tags.remove('Info') for tags in df['tags'] if 'Info' in tags]
-[tags.remove(val) for tags in df['tags'] for val in tags if 
-    'Road' in val or 
-    'Yards' in val or 
-    'Avenue' in val or 
-    'Street' in val or 
-    'Rd' in val or 
-    'Place' in val or 
-    'Walk' in val or 
-    'Lane' in val]
+
+rawtagslist = []
+
+for lis in df['tags']:
+    for string in lis:
+        if string in rawtagslist:
+            pass
+        elif 'Info' in string or 'View map' in string or 'Delivered by' in string or 'order' in string or 'Editions' in string:
+            pass        
+        elif ' ' in string:
+            if any(chr.isdigit() for chr in string) == True:
+                pass
+            else:
+                rawtagslist.append(string)                
+        else:
+            rawtagslist.append(string)
+
+print(rawtagslist)
+
+for tags in df['tags']:
+    for string in tags:
+        if string in rawtagslist:
+            pass
+        else: 
+            tags.remove(string)
+
+[tags.remove('View map') for tags in df['tags'] if 'View map' in tags] 
+ 
+[tags.remove(val) for tags in df['tags'] for val in tags if 'Editions' in val or 'delivered by' in val] 
+
 
 engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
 df.to_sql(f'{address}',engine, if_exists='replace')
-# %%
 
 # %%
