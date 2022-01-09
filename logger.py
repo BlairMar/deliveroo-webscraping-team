173d1aa292b8fe0b252cea4d12f8ce23e6b0886c
Hesham Haikal <heshamrhaikal@gmail.com>
2022-01-09 19:02:31 +0000

    Adds sentry log function

--- logger.py
+++ logger.py
@@ -1,10 +1,14 @@
 import sentry_sdk
 
-sentry_sdk.init(
-    "https://d2744aa667304febbb8766ca55f650f8@o1086610.ingest.sentry.io/6098931",
+def set_up_logger(dsn, env='staging'):
+    sentry_sdk.init(
+        dsn,
+        environment=env
+    )
 
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0
-)
+def log(level, message):
+    lvl = level
+    if level not in ['info', 'warn', 'error', 'fatal']:
+        lvl = 'debug'
+    sentry_sdk.capture_message(lvl, message)
+    print(f'{lvl.upper()}: {message}')
