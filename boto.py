60821063b88aa59a4d24d2bb004f57ea22241bac
Hesham Haikal <heshamrhaikal@gmail.com>
2022-01-17 19:04:16 +0000

    uploads multiple files

--- boto.py
+++ boto.py
@@ -7,18 +7,17 @@ class Upload:
     def __init__(self):
         self.s3_client = boto3.client('s3')
 
-    def uploadfile(self, address):
-        self.directory = os.fsencode(f'{address}/images')
-    
-        for image in os.listdir(self.directory):
-            try:
-                self.imagename = os.fsdecode(image) 
-                # print(os.path.join(directory, filename))
-                for self.im in self.imagename:
-                    self.response = self.s3_client.upload_file(f'{self.im}', 'deliveroobucket', f'{self.im}')
-            except:
-                print('excepted')
-                pass
+    def uploadfile(self, path):
+        # self.directory = os.fsencode(f'{address}/images')
+        bucketname = 'deliveroobucket'
+        
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                try:
+                    self.s3_client.upload_file(os.path.join(root, file), bucketname, file)
+                except:
+                    print('excepted')
+                    pass
 
     def viewbucket(self):
         self.s3 = boto3.resource('s3')
@@ -30,7 +29,7 @@ class Upload:
 #%%
 
 test1 = Upload()
-test1.uploadfile('data/SW1A 0AA')
+test1.uploadfile('data/')
 
 
 # %%
